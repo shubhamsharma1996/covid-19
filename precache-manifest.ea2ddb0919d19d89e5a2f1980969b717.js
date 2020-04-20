7ed69f74dad029f9004a6e896ab978d3c6cfc312
= <=>
2020-04-20 18:55:16 +0530

    Updates

---
+++ precache-manifest.ea2ddb0919d19d89e5a2f1980969b717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8e442d6ac40718c2ea4757e904e837",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "4604872eb670194d4e3d",
+    "url": "/covid-19/static/css/main.894b274c.chunk.css"
+  },
+  {
+    "revision": "5af91b58abfd0cfb401c",
+    "url": "/covid-19/static/js/2.ed2abb49.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.ed2abb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4604872eb670194d4e3d",
+    "url": "/covid-19/static/js/main.d582b13b.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
